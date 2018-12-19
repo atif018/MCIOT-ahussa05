c7187b8fb1f1d8c833a152853d1757b15ced28e6
Atif Hussain <atif018@gmail.com>
2018-12-19 15:20:50 +1100

    Updated permissions

--- app/src/main/java/com/example/sephiros/wifilogger/MainActivity.java
+++ app/src/main/java/com/example/sephiros/wifilogger/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sephiros.wifilogger;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    WifiManager wifiManager;
     String stopLog = "Stop Logging"; // Use this string for setting text on the Logging button
     String startLog = "Start Logging"; // Use this string for setting text on the Logging button
 
@@ -15,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
 
     // This method will be used when the start/stop logging button is pressed.
@@ -28,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
             logButton.setText(stopLog);
         } else { // If the text in logButtonStatus is not "Start Logging", then the text is changed to the string in startLog.
             logButton.setText(startLog);
+            return;
         }
+        wifiLog();
 
     }
 
@@ -38,6 +49,48 @@ public class MainActivity extends AppCompatActivity {
 
         Intent settingsIntent = new Intent(this, SettingsActivity.class);
         startActivity(settingsIntent);
+
+    }
+
+    public void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+    }
+
+    private void scanFailure() {
+        List<ScanResult> results = wifiManager.getScanResults();
+    }
+
+    // Used https://developer.android.com/guide/topics/connectivity/wifi-scan as a reference for the method below
+
+    private void wifiLog() {
+        wifiManager.startScan();
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false)
+                if (success) {
+                    scanSuccess();
+                } else {
+                    // scan failure handling
+                    scanFailure();
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
+
+        boolean success = wifiManager.startScan();
+        if (!success) {
+            // scan failure handling
+            scanFailure();
+        }
+
+
+
 
     }
     
