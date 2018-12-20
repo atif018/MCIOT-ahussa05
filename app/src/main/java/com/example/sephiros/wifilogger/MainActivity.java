5978e9f08b5c75796c471bc5288aa1404c465454
Atif Hussain <atif018@gmail.com>
2018-12-20 22:01:21 +1100

    Added Wifi Scanner, Broadcast and some other features

--- app/src/main/java/com/example/sephiros/wifilogger/MainActivity.java
+++ app/src/main/java/com/example/sephiros/wifilogger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sephiros.wifilogger;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,38 +11,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    WifiManager wifiManager;
-    String stopLog = "Stop Logging"; // Use this string for setting text on the Logging button
-    String startLog = "Start Logging"; // Use this string for setting text on the Logging button
+    private WifiManager wifiScanner;
+    private String stopLog = "Stop Logging"; // Use this string for setting text on the Logging button
+    private String startLog = "Start Logging"; // Use this string for setting text on the Logging button
+    TextView testView;
+    IntentFilter intentFilter;
+    List<ScanResult> listOfAps;
+    BroadcastReceiver wifiScanReceiver;
+
+    // Used https://developer.android.com/guide/topics/connectivity/wifi-scan as a reference when
+    // customising the onCreate method
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        testView = (TextView) findViewById(R.id.scannedWifi);
+        wifiScanner = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        Button startButtonScan = (Button)findViewById(R.id.scanWifi_button);
+        startButtonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiScanner.setWifiEnabled(true);
+                wifiScanner.startScan();
+            }
+        });
+
+        wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                listOfAps = wifiScanner.getScanResults();
+                /*if (listOfAps.size() > 0) {
+                    Toast.makeText(getApplicationContext(), "We're populating the list", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "List is not being populated!!!!", Toast.LENGTH_SHORT).show();
+                }*/
+
+                }
+            };
+
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
+
     }
 
     // This method will be used when the start/stop logging button is pressed.
     // Used https://developer.android.com/reference/android/widget/Button as a reference
 
-    public void startLog(View view) {
-
-        final Button logButton = findViewById(R.id.logging_button); // Create Button object using the logging_button state attributes
-        String logButtonStatus = logButton.getText().toString(); // Returns what text the logButton object has when the button is clicked, converts to a string and then stores in logButtonStatus
-        if (logButtonStatus.equals(startLog)) { // Checks if the text in logButtonStatus equals "Start Logging" and, if it is, changes the text to "Stop Logging" for when the user wants to stop
-            logButton.setText(stopLog);
-        } else { // If the text in logButtonStatus is not "Start Logging", then the text is changed to the string in startLog.
-            logButton.setText(startLog);
-            return;
-        }
-        wifiLog();
-
-    }
 
     // This method will be used when the Settings button is pressed.
 
@@ -52,46 +77,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void scanSuccess() {
-        List<ScanResult> results = wifiManager.getScanResults();
-    }
-
-    private void scanFailure() {
-        List<ScanResult> results = wifiManager.getScanResults();
-    }
-
-    // Used https://developer.android.com/guide/topics/connectivity/wifi-scan as a reference for the method below
-
-    private void wifiLog() {
-        wifiManager.startScan();
-
-        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context c, Intent intent) {
-                boolean success = intent.getBooleanExtra(
-                        WifiManager.EXTRA_RESULTS_UPDATED, false)
-                if (success) {
-                    scanSuccess();
-                } else {
-                    // scan failure handling
-                    scanFailure();
-                }
-            }
-        };
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
-
-        boolean success = wifiManager.startScan();
-        if (!success) {
-            // scan failure handling
-            scanFailure();
-        }
-
-
-
-
-    }
-    
 }
